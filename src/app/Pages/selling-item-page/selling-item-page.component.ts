2a50a66c51da9ffde68a04ba504a48270b44b3b4
Valentin <tolstoivalek@yandex.ru>
2025-05-03 15:34:52 +0300

    Selling item page updated.

--- src/app/Pages/selling-item-page/selling-item-page.component.ts
+++ src/app/Pages/selling-item-page/selling-item-page.component.ts
@@ -4,6 +4,8 @@ import {CustomHeaderComponent} from '../../common-ui/custom-header/custom-header
 import {FooterComponent} from '../../common-ui/footer/footer.component';
 import {NgbCarousel, NgbSlide} from '@ng-bootstrap/ng-bootstrap';
 import {MatButton} from '@angular/material/button';
+import {ProductsServiceService} from '../../Data/Services/products-service.service';
+import {ProductsImageService} from '../../Data/Services/products-image.service';
 
 @Component({
   selector: 'app-selling-item-page',
@@ -20,10 +22,35 @@ import {MatButton} from '@angular/material/button';
 export class SellingItemPageComponent {
   @Input("Id") Id : number = -1;
 
-  constructor(router: Router, private fb : ActivatedRoute) {
+  Title : string = '';
+  Price : number = 0.0;
+  Description : string = '';
+  CountInStock : number = 0;
+
+  protected Photos: string[] = [];
+
+  constructor(router: Router, private fb : ActivatedRoute, private itemService: ProductsServiceService,
+              private imgService: ProductsImageService) {
     fb.params.subscribe(params => {this.Id = params["id"];});
     if (this.Id == -1) {
       router.navigateByUrl('/error');
     }
+
+    itemService.getProductById(this.Id).then((product) => {
+      product.subscribe(product => {
+        this.Title = product.title;
+        this.Price = product.price;
+        this.Description = product.description;
+        this.CountInStock = product.countInStock;
+      })
+    });
+
+    imgService.GetProductImages(this.Id).then((images) => {
+      images.subscribe(image => {
+        image.forEach(image => {
+          this.Photos.push(image.data);
+        })
+      })
+    })
   }
 }
