d464c0c2d875dd62e149e8b9e8c33e94fc458cd5
ValentinTolstikov <tolstoivalek@yandex.ru>
2025-05-06 17:33:02 +0300

    Added mouse hover effects to product card component. Added redirections to category pages.

--- src/app/common-ui/product-card/product-card.component.ts
+++ src/app/common-ui/product-card/product-card.component.ts
@@ -1,9 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {category} from '../../Data/Interfaces/category-card.interface';
+import {Router} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
@@ -12,10 +16,17 @@ export class ProductCardComponent implements OnInit {
   @Input("Category") Category: category|null = null;
   path = '/Assets/Imgs/';
 
+  constructor(private router: Router) {
+  }
+
   ngOnInit(): void {
     if (this.Category != null)
     {
       this.Category.image = this.path + this.Category.image;
     }
+  }
+
+  GotoCategory($event: MouseEvent) {
+    this.router.navigateByUrl('/category/'+ this.Category?.route);
   }
 }
